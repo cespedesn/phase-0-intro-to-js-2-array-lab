4aec4ed2a3afec7668a00b831ca12e1892c5fe8b
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-05-25 12:00:51 -0400

    Completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats= [
+    "Milo", 
+    "Otis", 
+    "Garfield"];
+
+function  destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+const copyCats = [...cats, "Broom"];
+
+function appendCat(name) {
+    return copyCats;
+}
+const newCopyCats = ["Arnold", ...cats];
+
+function prependCat(name) {
+    return newCopyCats;
+}
+
+function removeLastCat(name) {
+    return cats.slice(0,2);
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
